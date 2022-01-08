b810cdbe85e513f98af3b155f6ac690534b93d17
ai-crypto <>
2022-01-08 23:12:38 +0900

    change in the spec

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,11 @@
-import { BigInt, BigDecimal, Address } from "@graphprotocol/graph-ts";
+import {
+  BigInt,
+  BigDecimal,
+  Address,
+  Bytes,
+  ethereum,
+} from "@graphprotocol/graph-ts";
+import { log } from "@graphprotocol/graph-ts";
 import {
   Deposited,
   Borrowed,
@@ -12,6 +19,7 @@ import { Transfer, cjpy } from "../generated/Cjpy/cjpy";
 import { Pledge, WorldState, Event } from "../generated/schema";
 import { yamato } from "../generated/yamato/yamato";
 import { LastGoodPriceUpdated } from "../generated/priceFeed/priceFeed";
+import {RedemptionReserveDeposited, RedemptionReserveUsed, SweepReserveDeposited, SweepReserveUsed} from "../generated/pool/pool";
 import { CJPY_ADDRESS } from "./utils/constant";
 
 export function handleDeposit(event: Deposited): void {
@@ -25,6 +33,7 @@ export function handleDeposit(event: Deposited): void {
     worldState.GRR = BigInt.fromI32(1);
     worldState.totalDebt = BigInt.fromI32(0);
     worldState.totalColl = BigInt.fromI32(0);
+    worldState.lastPrice = BigInt.fromI32(0);
   }
   deposit.ethAmount = event.params.ethAmount;
   deposit.address = event.transaction.from;
@@ -38,7 +47,6 @@ export function handleDeposit(event: Deposited): void {
     pledge = new Pledge(event.transaction.from.toHex());
     pledge.borrowedCjpyAmount = BigInt.fromI32(0);
     pledge.ethAmount = BigInt.fromI32(0);
-    pledge.withdrawLocks = BigInt.fromI32(0);
   }
   pledge.ethAmount = pledge.ethAmount.plus(event.params.ethAmount);
   pledge.save();
@@ -58,9 +66,6 @@ export function handleBorrow(event: Borrowed): void {
   let pledge = Pledge.load(event.transaction.from.toHex());
   pledge.borrowedCjpyAmount = event.params.cjpyAmount.plus(
     pledge.borrowedCjpyAmount || BigInt.fromI32(0)
-  );
-  pledge.withdrawLocks = event.block.timestamp.plus(
-    BigInt.fromI32(60 * 60 * 24 * 3)
   );
   pledge.save();
 }
@@ -102,8 +107,8 @@ export function handleRedeem(event: Redeemed): void {
   const worldState = WorldState.load(CJPY_ADDRESS);
   if (redeem === null) {
     redeem = new Event(event.transaction.hash.toHexString());
+    redeem.date = event.block.timestamp;
   }
-  redeem.date = event.block.timestamp;
   redeem.cjpyAmount = event.params.cjpyAmount;
   redeem.ethAmount = event.params.ethAmount;
   redeem.address = event.transaction.from;
@@ -111,15 +116,23 @@ export function handleRedeem(event: Redeemed): void {
   worldState.totalDebt = worldState.totalDebt.minus(event.params.cjpyAmount);
   worldState.totalColl = worldState.totalColl.minus(event.params.ethAmount);
   worldState.save();
+  const redeemedAddressList: Bytes[] = [];
   const pledgesOwner = event.params.pledgesOwner;
-  pledgesOwner.forEach((ownerAddress) => {
-    let pledge = Pledge.load(ownerAddress.toString());
-    const yamatoContract = yamato.bind(event.address);
-    const pledgeInfo = yamatoContract.getPledge(event.transaction.from);
-    pledge.borrowedCjpyAmount = pledgeInfo.debt;
-    pledge.ethAmount = pledgeInfo.coll;
-    pledge.save();
-  });
+  for (let i = 0; i < pledgesOwner.length; i++) {
+    const ownerAddress = pledgesOwner[i];
+    if (
+      ownerAddress.toHexString() != "0x0000000000000000000000000000000000000000"
+    ) {
+      redeemedAddressList.push(ownerAddress);
+      const pledge = new Pledge(ownerAddress.toHexString());
+      const yamatoContract = yamato.bind(event.address);
+      const pledgeInfo = yamatoContract.getPledge(ownerAddress);
+      pledge.borrowedCjpyAmount = pledgeInfo.debt;
+      pledge.ethAmount = pledgeInfo.coll;
+      pledge.save();
+    }
+  }
+  redeem.redeemedAddressList = redeemedAddressList;
   redeem.save();
 }
 
@@ -127,6 +140,7 @@ export function handleRedeemMeta(event: RedeemedMeta): void {
   let redeem = Event.load(event.transaction.hash.toHex());
   if (redeem === null) {
     redeem = new Event(event.transaction.hash.toHex());
+    redeem.date = event.block.timestamp;
   }
   redeem.price = event.params.price;
   redeem.isCoreRedemption = event.params.isCoreRedemption;
@@ -147,22 +161,37 @@ export function handleSweep(event: Swept): void {
   worldState.totalDebt = worldState.totalDebt.minus(event.params.cjpyAmount);
   worldState.save();
   const pledgesOwner = event.params.pledgesOwner;
-  pledgesOwner.forEach((ownerAddress) => {
-    let pledge = Pledge.load(ownerAddress.toHex());
-    const yamatoContract = yamato.bind(event.address);
-    const pledgeInfo = yamatoContract.getPledge(event.transaction.from);
-    pledge.borrowedCjpyAmount = pledgeInfo.debt;
-    pledge.ethAmount = pledgeInfo.coll;
-    pledge.save();
-  });
+  const sweepedAddressList: Bytes[] = [];
+  for (let i = 0; i < pledgesOwner.length; i++) {
+    const ownerAddress = pledgesOwner[i];
+    if (
+      ownerAddress.toHexString() != "0x0000000000000000000000000000000000000000"
+    ) {
+      sweepedAddressList.push(ownerAddress);
+      const pledge = Pledge.load(ownerAddress.toHexString());
+      const yamatoContract = yamato.bind(event.address);
+      const pledgeInfo = yamatoContract.getPledge(ownerAddress);
+      pledge.borrowedCjpyAmount = pledgeInfo.debt;
+      pledge.ethAmount = pledgeInfo.coll;
+      pledge.save();
+    }
+  }
+  sweep.sweepedAddressList = sweepedAddressList;
   sweep.save();
 }
 
 export function handleMintAndBurn(event: Transfer): void {
-  let worldState = WorldState.load(event.address.toHexString());
+  let worldState = WorldState.load(CJPY_ADDRESS);
   const cjpyContract = cjpy.bind(event.address);
   if (!worldState) {
     worldState = new WorldState(event.address.toHex());
+    worldState.totalDebt = BigInt.fromI32(0);
+    worldState.totalColl = BigInt.fromI32(0);
+    worldState.MCR = BigInt.fromI32(110);
+    worldState.RRR = BigInt.fromI32(80);
+    worldState.SRR = BigInt.fromI32(20);
+    worldState.GRR = BigInt.fromI32(1);
+    worldState.lastPrice = BigInt.fromI32(0);
   }
   worldState.totalSupplyCjpy = cjpyContract.totalSupply();
   worldState.save();
@@ -178,9 +207,9 @@ export function handleFetchPrice(event: LastGoodPriceUpdated): void {
     worldState.RRR = BigInt.fromI32(80);
     worldState.SRR = BigInt.fromI32(20);
     worldState.GRR = BigInt.fromI32(1);
+    worldState.lastPrice = event.params._lastGoodPrice;
   }
-  worldState.lastPrice = event.params._lastGoodPrice;
-  if (worldState.lastPrice) {
+  if (event.params._lastGoodPrice.notEqual(worldState.lastPrice) && worldState.lastPrice.notEqual(BigInt.fromI32(0))) {
     const old_price = BigDecimal.fromString(worldState.lastPrice.toString());
     const new_price = BigDecimal.fromString(
       event.params._lastGoodPrice.toString()
@@ -190,5 +219,18 @@ export function handleFetchPrice(event: LastGoodPriceUpdated): void {
       .div(old_price)
       .times(BigDecimal.fromString("100"));
   }
+  worldState.lastPrice = event.params._lastGoodPrice;
+  worldState.save();
+}
+
+export function handleRedemptionReserve(event: RedemptionReserveDeposited): void {
+  let worldState = WorldState.load(CJPY_ADDRESS);
+  worldState.redemptionReserve = event.params.param2;
+  worldState.save();
+}
+
+export function handleSweepReserve(event: SweepReserveDeposited): void {
+  let worldState = WorldState.load(CJPY_ADDRESS);
+  worldState.sweepReserve = event.params.param2;
   worldState.save();
 }
